bb00f70f6ab429d39de9f06af6c5cca9e7b9bdc1
YouYou-tol <v1p2n3tool@qq.com>
2025-09-12 12:32:26 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111.229.174.167"];
+pub const RS_PUB_KEY: &str = "KR67aZ2YV3x7u6YFQPl1RjdLXWVikmWBsYF63JlaV9M=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
